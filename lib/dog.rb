fd694b957150c90527236502d1168f31fde152fd
kayla <kay.ann2000@gmail.com>
2022-04-27 10:06:58 -0600

    commit tree 306c2198fdd97b44941ccf8d23bc97b1ff6610a2

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+  #  Define a name getter and a name= setter method for the instance variable @name.
+#Define a breed getter and a breed= setter method for the instance variable @breed.
 
+def name=(name)
+    @name = name
+end 
+def name 
+    @name
+end
+def breed=(breed)
+    @breed = breed
+end
+def breed
+    @breed
+end
 end
